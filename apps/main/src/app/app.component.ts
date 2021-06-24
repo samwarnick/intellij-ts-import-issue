7dd63a198501bc7836c9f7a26a2ebbe2a33050e3
Sam Warnick <swarnick@simplifile.com>
2021-06-24 10:47:20 -0400

    initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intellij-ts-import-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
